8228132dffb8ad160452a6ec9981c4e17858e8ee
= <=>
2018-10-01 18:26:16 -0300

    merge

--- src/filters/index.ts
+++
@@ -1,5 +0,0 @@
-import angular = require('angular')
-
-export default angular
-  .module('product.filters', [])
-  .name
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import * as angular from 'angular'
 import components from './components'
-import filters from './filters'
 
 const module = angular
-  .module('product', [
+  .module('demo', [
     components,
-    filters,
   ])
 
 angular.bootstrap(document, [module.name])
