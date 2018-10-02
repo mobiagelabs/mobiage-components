adf90b2071afa4d2c0992fce50d4b39f4001d769
= <=>
2018-10-01 22:02:33 -0300

    upload to firebase

---
+++ src/components/image-upload/components/image-upload-no-image/image-upload-no-image.ts
@@ -0,0 +1,12 @@
+import './image-upload-no-image.scss'
+import template from './image-upload-no-image.html'
+
+const imageUploadNoImage = {
+    bindings: {
+        width: '@',
+        height: '@',
+    },
+    template,
+}
+
+export { imageUploadNoImage }
--- src/components/image-upload/index.ts
+++ src/components/image-upload/index.ts
@@ -2,12 +2,14 @@ import * as angular from 'angular'
 import { imageUpload } from './image-upload'
 import { imageUploadMain } from './components/image-upload-main/image-upload-main'
 import { imageUploadChildren } from './components/image-upload-children/image-upload-children'
+import { imageUploadNoImage } from './components/image-upload-no-image/image-upload-no-image'
 
 const imageUploadModule = angular
     .module('mbg.components.imageUpload', [])
     .component('mbgImageUpload', imageUpload)
     .component('mbgImageUploadMain', imageUploadMain)
     .component('mbgImageUploadChildren', imageUploadChildren)
+    .component('mbgImageUploadNoImage', imageUploadNoImage)
     .name
 
 export * from './interfaces'
