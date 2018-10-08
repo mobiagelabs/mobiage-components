e14876fc146cfc5a2e0b8850f24f556e41bf0e6f
lucaskolecha <lkolecha@hotmail.com>
2018-10-08 18:31:27 -0300

    module name

--- src/components/mbg-input-tags/index.ts
+++ src/components/mbg-input-tags/index.ts
@@ -2,7 +2,7 @@ import * as angular from 'angular'
 import { mbgInputTags } from './mbg-input-tags'
 
 const mbgInputTagsModule = angular
-    .module('mbg.components.mbgInputText', [])
+    .module('mbg.components.mbgInputTags', [])
     .component('mbgInputTags', mbgInputTags)
     .name
 
