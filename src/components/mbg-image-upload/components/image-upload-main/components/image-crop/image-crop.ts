11b027e53d632a55bf97fd9f8a193d6a3826ad2b
lucaskolecha <lkolecha@hotmail.com>
2018-10-26 08:37:46 -0300

    Adicionando crop e zoom no image-upload

---
+++ src/components/mbg-image-upload/components/image-upload-main/components/image-crop/image-crop.ts
@@ -0,0 +1,77 @@
+import './image-crop.scss'
+import 'croppie/croppie.css'
+import { Croppie } from 'croppie'
+import template from './image-crop.html'
+import * as angular from 'angular'
+import { ImageUploadConfig } from '../../../../interfaces'
+
+class ImageCropController {
+    private inputElement: HTMLInputElement
+    private config: ImageUploadConfig
+    private pendentCrop: any
+    private width: number
+    private height: number
+    private upload: Function
+    private element: any
+    private crop: any
+    private editImage: any
+
+    constructor(public $scope, public $element, public $timeout) {
+    }
+
+    $onInit() {
+        this.element = this.$element.find('.crop')[0]
+        this.setImageCrop()
+    }
+
+    setImageCrop() {
+        this.editImage = this.pendentCrop
+        this.createCrop()
+    }
+
+    createCrop() {
+        const defaultCrop = {
+            viewport: {
+                width: this.width,
+                height: this.height,
+                // type: this.params.type == 'circle' ? 'circle' : 'square'
+            },
+            zoomOnWheel: true,
+            enableOrientation: true,
+            checkCrossOrigin: false,
+        }
+
+        const config = Object.assign(defaultCrop)
+        this.crop = new Croppie(this.element, config)
+        this.crop.bind({ url: this.editImage }).then()
+    }
+
+    closeCrop() {
+        this.$timeout(() => {
+            delete this.pendentCrop
+        })
+    }
+
+    acceptCrop() {
+        this.crop.result({ type: 'base64' }).then((resp) => {
+            this.upload(resp)
+        })
+    }
+
+}
+
+ImageCropController.$inject = ['$scope', '$element', '$timeout']
+
+const imageCrop = {
+    bindings: {
+        pendentCrop: '=',
+        upload: '=',
+        config: '=',
+        width: '=',
+        height: '='
+    },
+    controller: ImageCropController,
+    template,
+}
+
+export { imageCrop }
