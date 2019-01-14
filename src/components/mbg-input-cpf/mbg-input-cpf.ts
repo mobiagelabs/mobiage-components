2de1b933ea6738640b35361a9bf11358f89068b2
Mateus Miranda de Almeida <info.mateusmiranda@gmail.com>
2019-01-14 14:51:33 -0200

    CPF correção

--- src/components/mbg-input-cpf/mbg-input-cpf.ts
+++ src/components/mbg-input-cpf/mbg-input-cpf.ts
@@ -7,7 +7,6 @@ class MbgInputCpfController {
     private ngRequired
     private ngDisabled
     private props
-    public valid
 
     constructor($scope, $element, $attrs) {
         if ($attrs.ngRequired === '') { this.ngRequired = true }
@@ -17,49 +16,14 @@ class MbgInputCpfController {
         }
     }
 
-    ngBlur(evt) {
-        this.valid = this.validCpf(evt.$event.target.value)
-        if (!this.valid) {
-            this.ngModel = null
-        }
-    }
-
-    calcDigits(digits, positions = 10, sumDigits = 0) {
-        digits = digits.toString()
-        for (let i = 0; i < digits.length; i++) {
-            sumDigits = sumDigits + (digits[i] * positions)
-            positions--
-            if (positions < 2) {
-                positions = 9
-            }
-        }
-        sumDigits = sumDigits % 11
-        if (sumDigits < 2) {
-            sumDigits = 0
-        } else {
-            sumDigits = 11 - sumDigits
-        }
-        let cpf = digits + sumDigits
-        return cpf
-    }
-
-    validCpf(value) {
-        let digits = value.substr(0, 9)
-        let firstCalc = this.calcDigits(digits)
-        let newCpf = this.calcDigits(firstCalc, 11)
-        if (newCpf === value) {
-            return true
-        } else {
-            return false
-        }
-    }
-
     onChange() {
         if (this.ngChange) {
             this.ngChange({})
         }
     }
+
 }
+
 MbgInputCpfController.$inject = ['$scope', '$element', '$attrs']
 
 const mbgInputCpf = {
