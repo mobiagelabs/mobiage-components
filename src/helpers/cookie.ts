b503c2e8fe307ff95810a0de0ad1f7f85044addc
lucaskolecha <lskolecha@gmail.com>
2019-02-06 10:45:53 -0200

    features ng-select

---
+++ src/helpers/cookie.ts
@@ -0,0 +1,43 @@
+export namespace MbgCookie {
+    const getKeyCookie = (key: string) => {
+        let user: any = sessionStorage.getItem('user')
+        if (user) {
+            user = JSON.parse(user)
+            return (user.organizationHierarchyCode || '') + 'preference-' + key
+        } else {
+            return 'preference-' + key
+        }
+    }
+    const getCookie = (key) => {
+        key = getKeyCookie(key)
+        const nameEQ = key + '='
+        const ca = document.cookie.split(';')
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i]
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1, c.length)
+            }
+            if (c.indexOf(nameEQ) === 0) {
+                return c.substring(nameEQ.length, c.length)
+            }
+        }
+        return null
+    }
+    export function set(key: string, val: any) {
+        const value = JSON.stringify({ value: val })
+        key = getKeyCookie(key)
+        let expires = ''
+        const date = new Date()
+        date.setTime(date.getTime() + (9999 * 24 * 60 * 60 * 1000))
+        expires = '; expires=' + date.toUTCString()
+        document.cookie = key + '=' + (value || '') + expires + '; path=/'
+    }
+    export function get(key: string) {
+        const value = getCookie(key)
+        try {
+            return JSON.parse(value).value
+        } catch (e) {
+            return null
+        }
+    }
+}
