62dd47be20e9080be6c9948899d7ee6de21963cf
Felipe Sabadini Facina <fsabadini@hotmail.com>
2019-05-07 18:03:50 -0300

    inputnumber correções

--- src/components/mbg-input-number/mbg-input-number.ts
+++ src/components/mbg-input-number/mbg-input-number.ts
@@ -2,61 +2,71 @@ import './mbg-input-number.scss'
 import template from './mbg-input-number.html'
 
 class MbgInputNumberController {
-    private ngChange
-    private ngModel
-    private ngRequired
-    private ngDisabled
-    private props
+  private ngChange
+  private ngModel
+  private ngRequired
+  private ngDisabled
+  private props
 
-    constructor(public $scope, public $element, public $attrs, public $compile, public $timeout) {
-        if ($attrs.ngRequired === '') { this.ngRequired = true }
-        if ($attrs.ngDisabled === '') { this.ngDisabled = true }
-        this.props = {
-            placeholder: $attrs.placeholder || '',
-            precision: $attrs.precision || 0,
-            allowNegative: $attrs.allowNegative ? JSON.parse($attrs.allowNegative) : false,
-            uiHideGroupSep: typeof $attrs.uiHideGroupSep !== undefined ? true : undefined
-        }
-        if ($attrs.min) { this.props.min = $attrs.min }
-        if ($attrs.max) { this.props.max = $attrs.max }
-    }
+  constructor(public $scope, public $element, public $attrs, public $compile, public $timeout) {}
 
-    $onInit() {
-        if (this.props.allowNegative) {
-            this.enableNegative()
-        }
-    }
+  $onInit() {
+    this.$timeout(() => {
+      if (this.$attrs.ngRequired === '') {
+        this.ngRequired = true
+      }
+      if (this.$attrs.ngDisabled === '') {
+        this.ngDisabled = true
+      }
+      this.props = {
+        placeholder: this.$attrs.placeholder || '',
+        precision: this.$attrs.precision || 0,
+        allowNegative: this.$attrs.allowNegative ? JSON.parse(this.$attrs.allowNegative) : false,
+        uiHideGroupSep: typeof this.$attrs.uiHideGroupSep !== undefined ? true : undefined
+      }
+      if (this.$attrs.min) {
+        this.props.min = this.$attrs.min
+      }
+      if (this.$attrs.max) {
+        this.props.max = this.$attrs.max
+      }
+      if (this.props.allowNegative) {
+        this.enableNegative()
+      }
+    })
+  }
 
-    enableNegative() {
-        this.$timeout(() => {
-            let input = this.$element.find('input').clone()
-            input.attr('ui-negative-number', 'true')
-            input = this.$compile(input)(this.$scope)
-            this.$element.find('input').replaceWith(input)
-        })
-    }
+  enableNegative() {
+    this.$timeout(() => {
+      let input = this.$element.find('input').clone()
+      input.attr('ui-negative-number', 'true')
+      input = this.$compile(input)(this.$scope)
+      this.$element.find('input').replaceWith(input)
+    })
+  }
 
-    onChange() {
-        if (this.ngChange) {
-            this.ngChange({})
-        }
+  onChange() {
+    if (this.ngChange) {
+      this.ngChange({})
     }
+  }
 
 }
+
 MbgInputNumberController.$inject = ['$scope', '$element', '$attrs', '$compile', '$timeout']
 
 const mbgInputNumber = {
-    bindings: {
-        ngModel: '=',
-        ngChange: '&?',
-        ngRequired: '=?',
-        ngDisabled: '=?',
-        ngBlur: '&?',
-        ngFocus: '&?',
-        positive: '=?'
-    },
-    template,
-    controller: MbgInputNumberController,
+  bindings: {
+    ngModel: '=',
+    ngChange: '&?',
+    ngRequired: '=?',
+    ngDisabled: '=?',
+    ngBlur: '&?',
+    ngFocus: '&?',
+    positive: '=?'
+  },
+  template,
+  controller: MbgInputNumberController,
 }
 
 export { mbgInputNumber }
