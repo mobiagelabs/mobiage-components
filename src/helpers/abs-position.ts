d00324e681061cdc8f16a50bb9d1a50b63245121
lucaskolecha <lskolecha@gmail.com>
2019-09-04 09:05:24 -0300

    Correção abs-position

--- src/helpers/abs-position.ts
+++ src/helpers/abs-position.ts
@@ -1,11 +1,23 @@
 export namespace AbsPosition {
-
     export function get(el: any) {
         const rect = el.getBoundingClientRect()
+        let zooms = []
+        while (el) {
+            const style = window.getComputedStyle(el)
+            zooms.push(style.getPropertyValue('zoom'))
+            el = el.offsetParent
+        }
+        zooms = zooms.filter((zoom) => zoom && Number(zoom) !== 1)
+        const value = zooms.reduce((v, zoom) => v + parseFloat(zoom), 0) / zooms.length * 100
+        if (!isNaN(value)) {
+            return {
+                top: (rect.top + window.pageYOffset) * (value / 100),
+                left: (rect.left + window.pageXOffset) * (value / 100),
+            }
+        }
         return {
-            top: rect.top + window.pageYOffset,
-            left: rect.left + window.pageXOffset,
+            top: (rect.top + window.pageYOffset),
+            left: (rect.left + window.pageXOffset),
         }
     }
-
-}
+ }
